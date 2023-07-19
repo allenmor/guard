6cbae185e78accb3ea1238f2bca2800d2d7fcbad
Allen Mordukhaev <allenmor93@gmail.com>
2023-07-19 17:51:24 -0400

    address added

--- src/components/Home/UserPage.js
+++ src/components/Home/UserPage.js
@@ -1,24 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { auth, db } from '../../firebase';
-import { setDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
-import './UserPage.css'
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { auth, db } from "../../firebase";
+import { setDoc, doc, getDoc, updateDoc } from "firebase/firestore";
+import "./UserPage.css";
 
 function UserPage() {
   const location = useLocation();
   const { email, name } = location.state || {};
   const now = new Date();
-  const formattedDate = `${now.toLocaleString('default', { month: 'short' })} ${now.getDate()} ${now.getFullYear()}`;
+  const formattedDate = `${now.toLocaleString("default", {
+    month: "short",
+  })} ${now.getDate()} ${now.getFullYear()}`;
   const timeString = `${now.getHours()}:${now.getMinutes()}`;
   const navigate = useNavigate();
-
+  const [geoLocation, setGeoLocation] = useState({
+    latitude: null,
+    longitude: null,
+    address: null,
+    error: null,
+  });
   const [clockInTime, setClockInTime] = useState(null);
   const [clockOutTime, setClockOutTime] = useState(null);
 
-
   useEffect(() => {
     const getUser = async () => {
-      const userRef = doc(db, 'users', name);
+      const userRef = doc(db, "users", name);
       const docSnap = await getDoc(userRef);
       if (docSnap.exists()) {
         const userData = docSnap.data();
@@ -34,26 +40,27 @@ function UserPage() {
     getUser();
   }, [name]);
 
-
   const handleLogout = () => {
     auth
       .signOut()
       .then(() => {
-        navigate('/');
+        navigate("/");
       })
       .catch((error) => {
-        console.error('Logout Error:', error);
+        console.error("Logout Error:", error);
       });
   };
 
   const handleClockIn = async () => {
     const now = new Date();
-    const formattedDate = `${now.toLocaleString('default', { month: 'short' })} ${now.getDate()} ${now.getFullYear()}`;
+    const formattedDate = `${now.toLocaleString("default", {
+      month: "short",
+    })} ${now.getDate()} ${now.getFullYear()}`;
     const timeString = `${now.getHours()}:${now.getMinutes()}`;
     console.log(formattedDate);
     setClockInTime(timeString);
 
-    const userRef = doc(db, 'users', name);
+    const userRef = doc(db, "users", name);
 
     const docSnap = await getDoc(userRef);
 
@@ -71,7 +78,9 @@ function UserPage() {
     } else {
       const userData = docSnap.data();
       let clockInsArray = userData.clockIns || [];
-      const existingClockIn = clockInsArray.find((clockIn) => clockIn.date === formattedDate);
+      const existingClockIn = clockInsArray.find(
+        (clockIn) => clockIn.date === formattedDate
+      );
 
       if (!existingClockIn) {
         // Create a new clockIn record if it doesn't exist for the date
@@ -89,18 +98,22 @@ function UserPage() {
 
   const handleClockOut = async () => {
     const now = new Date();
-    const formattedDate = `${now.toLocaleString('default', { month: 'short' })} ${now.getDate()} ${now.getFullYear()}`;
+    const formattedDate = `${now.toLocaleString("default", {
+      month: "short",
+    })} ${now.getDate()} ${now.getFullYear()}`;
     const timeString = `${now.getHours()}:${now.getMinutes()}`;
 
     setClockOutTime(timeString);
 
-    const userRef = doc(db, 'users', name);
+    const userRef = doc(db, "users", name);
     const docSnap = await getDoc(userRef);
 
     if (docSnap.exists()) {
       const userData = docSnap.data();
       let clockInsArray = userData.clockIns || [];
-      const existingClockIn = clockInsArray.find((clockIn) => clockIn.date === formattedDate);
+      const existingClockIn = clockInsArray.find(
+        (clockIn) => clockIn.date === formattedDate
+      );
 
       if (existingClockIn) {
         // Update the existing clockIn record with clockOutTime
@@ -113,15 +126,63 @@ function UserPage() {
     }
   };
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setGeoLocation((prevState) => ({
+          ...prevState,
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }));
+
+        // Call Google's Geocoding API
+        fetch(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=${process.env.REACT_APP_GEO_LOCATION_KEY}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.results && data.results[0]) {
+              setGeoLocation((prevState) => ({
+                ...prevState,
+                address: data.results[0].formatted_address,
+              }));
+            } else {
+              throw new Error("No results returned from geocoding API");
+            }
+          })
+          .catch((error) =>
+            setGeoLocation((prevState) => ({
+              ...prevState,
+              error: error.message,
+            }))
+          );
+      },
+      (error) =>
+        setGeoLocation((prevState) => ({
+          ...prevState,
+          error: error.message,
+        })),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }, []);
+
   return (
     <div className="user-page">
       <h1 className="welcome">Welcome, {name}</h1>
+      <h1 className="welcome">Address: {geoLocation.address}</h1>
       <p className="date">Current Date: {formattedDate}</p>
       <p className="time">Current Time: {timeString}</p>
       <p className="email">Email: {email}</p>
-      <button className="logout-btn" onClick={handleLogout}>Logout</button>
-      <button className="clock-in-btn" onClick={handleClockIn}>Clock In</button>
-      <button className="clock-out-btn" onClick={handleClockOut}>Clock Out</button>
+      <button className="logout-btn" onClick={handleLogout}>
+        Logout
+      </button>
+      <button className="clock-in-btn" onClick={handleClockIn}>
+        Clock In
+      </button>
+      <button className="clock-out-btn" onClick={handleClockOut}>
+        Clock Out
+      </button>
       <p className="clock-in-time">Clock In Time: {clockInTime}</p>
       <p className="clock-out-time">Clock Out Time: {clockOutTime}</p>
     </div>
