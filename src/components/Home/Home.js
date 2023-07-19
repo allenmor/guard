9476781f47ec51f52f5704e6566aa526d9d9cfc6
Allen Mordukhaev <allenmor93@gmail.com>
2023-07-18 23:06:30 -0400

    init

---
+++ src/components/Home/Home.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './Home.css'
+
+function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login authentication logic here
+  };
+
+  return (
+    <div className='home-div'>
+      <h1>Security Check In</h1>
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+}
+
+export default Home;
